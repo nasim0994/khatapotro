92c935ad11d7b636ef82b6c266c53a0078c9d3d1
nasim0994 <nasim0994@gmail.com>
2026-06-14 10:42:25 +0600

    setup and onboarding screen complete

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#2F80ED",
+          glow: "#4D96FF",
+        },
+        secondary: {
+          DEFAULT: "#0B0E14",
+          card: "#161B26",
+          surface: "#1F2633",
+        },
+        text: {
+          main: "#FFFFFF",
+          muted: "#828282",
+        },
+        accent: {
+          income: "#219653",
+          expense: "#EB5757",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
